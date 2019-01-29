63a38cd224f8040912e45f8c98eea5e183e8a140
etudiant <Vous@exemple.com>
2019-01-29 14:33:14 +0100

    j'ai fait une fonction en def  pour passer le  truc en grs

--- Projet_CDA.py
+++ Projet_CDA.py
@@ -7,3 +7,11 @@ Created on Tue Jan 29 14:06:58 2019
 """
 
 sdsd
+def BoldWord(mot, texte):
+ 
+    long = len(mot)
+    for i in range(0, len(texte)):
+        sel = texte[i:i+long]
+        if sel.lower() == mot.lower() and texte[i-3:i]!="<b>":
+            return BoldWord(mot, texte.replace(sel, "<b>%s</b>" % sel))
+    return texte
